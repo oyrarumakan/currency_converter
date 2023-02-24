5f9b3c03fbce720fddaaa67148a61c12dfc51032
oyrarumakan <ryo19911030@hotmail.co.jp>
2023-02-24 18:45:45 +0900

    :new: Add convert method(usd_to_jpy) #1

--- lib/currency_converter.rb
+++ lib/currency_converter.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "currency_converter/version"
+require "currency_converter/convert"
 
 module CurrencyConverter
   class Error < StandardError; end
---
+++ lib/currency_converter/convert.rb
@@ -0,0 +1,7 @@
+module CurrencyConverter
+  class Convert
+    def usd_to_jpy(jpy)
+      jpy * 130
+    end
+  end
+end
